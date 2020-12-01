10bc6cf69e1909f326950aa8c8e4f451224c25ab
LudwinD <ludwinburrion@gmail.com>
2020-11-30 23:06:25 -0600

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('CICD desde GitHub!'));
+app.get('/', (req, res) => res.send('CICD desde GitHub! Manual'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
